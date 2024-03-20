a48becf5d730c50e0935ccf29a7caf9e42be34ab
Andy <andy.tonthat14@gmail.com>
2024-03-19 20:30:18 -0700

    usage message and input type validation

---
+++ boardMaker.py
@@ -0,0 +1,82 @@
+#---[ Global Imports ]---------------------------------------------------------
+import sys
+import random
+from   string import digits
+
+##--[ Global Imports ]---------------------------------------------------------
+
+
+#---[ Board Generator Class ]--------------------------------------------------
+class ArgvHandler:
+    def __init__(self) -> None:
+        self._EXPECTED_ARG_COUNT = 6
+        self._argv = sys.argv
+
+        return
+
+    def getArgsTup(self) -> tuple[str, int, int, int, int]:
+        self.validateArgs()
+
+        return (
+            self._argv[1],
+            int(self._argv[2]),
+            int(self._argv[3]),
+            int(self._argv[4]),
+            int(self._argv[5]),
+        )
+
+    def validateArgs(self) -> None:
+        invalidArgCount = len(self._argv) != self._EXPECTED_ARG_COUNT
+
+        if invalidArgCount:
+            self.printUsageMessage()
+            raise SystemExit
+        
+        # validate numBoards, rows, cols, & numHints are ints
+        invalidTypes = False
+        for arg in self._argv[2:]:
+            if not arg.isnumeric():
+                invalidTypes = True
+                break
+        
+        if invalidTypes:
+            self.printUsageMessage()
+            raise SystemExit
+
+    def printUsageMessage(self) -> None:
+        print("Usage:")
+        print(f"$ boardMaker fileName numBoards rows cols numHints\n")
+        print(f"{'':2s}{'fileName ':<15s}Base file name for collection of game boards storage")
+        print(f"{'':2s}{'numBoards':<15s}Number of files to create storing each board")
+        print(f"{'':2s}{'rows     ':<15s}Row count for each cell")
+        print(f"{'':2s}{'cols     ':<15s}Column count for each cell")
+        print(f"{'':2s}{'numHints ':<15s}Total number of revealed values at the start of the game")
+
+        return
+
+
+class BoardGenerator:
+    def __init__(self) -> None:
+        pass
+    
+
+##--[ Board Generator Class ]--------------------------------------------------
+    
+
+#---[ Main Function ]----------------------------------------------------------
+def main() -> None:
+    argvHandler = ArgvHandler()
+    argvCorrect = argvHandler.getArgsTup()
+
+    generator = BoardGenerator()
+    
+    return
+
+##--[ Main Function ]----------------------------------------------------------
+
+
+#---[ Entry ]------------------------------------------------------------------
+if __name__ == "__main__":
+    main()
+
+##--[ Entry ]------------------------------------------------------------------
